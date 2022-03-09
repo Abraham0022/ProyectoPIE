be9587755bbcac0d6ef7659de58157cbefe601c4
Ruben <abrahamclase@gmail.com>
2022-03-09 18:40:22 +0100

    18:39

---
+++ app/src/main/java/com/example/proyectopie/ActivityPreguntas.java
@@ -0,0 +1,31 @@
+package com.example.proyectopie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityPreguntas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preguntas);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("me llamo pedro");
+        setSupportActionBar(myToolbar);
+
+        Intent intent = getIntent();
+        String nombre = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.tv_nombre);
+        textView.setText(nombre);
+
+        String hola ="hola Abraham!!!";
+
+
+    }
+}
---
+++ app/src/main/java/com/example/proyectopie/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.proyectopie;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_iniciar;
+    public static final String EXTRA_MESSAGE = "com.example.proyectopie.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*btn_iniciar = (Button) findViewById(R.id.btn_iniciar);
+        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent empezarTest=new Intent(MainActivity.this, ActivityPreguntas.class);
+                startActivity(empezarTest);
+            }
+        });*/
+    }
+
+    public void empezarTest(View v){
+        Intent intent = new Intent(this, ActivityPreguntas.class);
+        EditText editText = (EditText) findViewById(R.id.et_nombre);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
