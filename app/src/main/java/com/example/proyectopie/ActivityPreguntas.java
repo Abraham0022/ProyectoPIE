fb3cb5bc1283d15c493ed0c1b62951a299e1c371
Abraham0022 <99471369+Abraham0022@users.noreply.github.com>
2022-03-10 20:15:52 +0100

    Preguntas con RadioButun
    Dejo funcionando el contador de respuestas correctas, tenemos que pasar la variable aciertos al próximo intent

--- app/src/main/java/com/example/proyectopie/ActivityPreguntas.java
+++ app/src/main/java/com/example/proyectopie/ActivityPreguntas.java
@@ -5,28 +5,107 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityPreguntas extends AppCompatActivity {
+    private BaseDatos baseDeDatos;
+    private int numPregunta=0, aciertos=0;
+    TextView preguntaTxt, txtPrueba;
+    RadioButton rbResp1, rbResp2, rbResp3;
+    RadioGroup rgGrupo;
+    Button btn_iniciar;
+    String respuestaCorrecta, strResp;
+    String nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        baseDeDatos=new BaseDatos();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        myToolbar.setTitle("me llamo pedro");
-        setSupportActionBar(myToolbar);
+
 
         Intent intent = getIntent();
-        String nombre = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        nombre = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        TextView textView = findViewById(R.id.tv_nombre);
-        textView.setText(nombre);
+        myToolbar.setTitle("Adelante "+ nombre);
+        setSupportActionBar(myToolbar);
 
-        String hola ="hola Abraham!!!";
-        String adios ="adiossssssss";
+        preguntaTxt = findViewById(R.id.tv_pregunta);
+        rbResp1 = findViewById(R.id.rbResp1);
+        rbResp2 = findViewById(R.id.rbResp2);
+        rbResp3 = findViewById(R.id.rbResp3);
+        cargarPregunta();
 
+        btn_iniciar = (Button) findViewById(R.id.btnComprobar);
+        btn_iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if(respuestaCorrecta==strResp){
+                    aciertos++;
+                }
+                numPregunta++;
+                rbResp1.setChecked(false);
+                rbResp2.setChecked(false);
+                rbResp3.setChecked(false);
+                cargarPregunta();
+
+            }
+        });
+
+        //txtPrueba=findViewById(R.id.txtPrueba);
+
+    }
+    protected void cargarPregunta(){
+        //Hemos de intentar que esto funcione en random
+
+        if (baseDeDatos.cantidadPreguntas()>numPregunta){
+            preguntaTxt.setText(baseDeDatos.obtenerPregunta(numPregunta));
+
+            rbResp1.setText(baseDeDatos.obtenerRespuesta1(numPregunta));
+            rbResp2.setText(baseDeDatos.obtenerRespuesta2(numPregunta));
+            rbResp3.setText(baseDeDatos.obtenerRespuesta3(numPregunta));
+            respuestaCorrecta = baseDeDatos.obtenerRespuestaCorrecta(numPregunta);
+
+        }else{
+         /*   Intent mostrarPuntuacion = new Intent (TestActivity.this, PuntuacionActivity.class);
+            mostrarPuntuacion.putExtra("nombre",nombreJugador);
+            mostrarPuntuacion.putExtra("puntos", puntuacion);
+            startActivity(mostrarPuntuacion);
+            finish();*/
+            preguntaTxt.setText("NO hay mas preguntas "+ nombre +" has acertado: " +aciertos);
+        }
+    }
+
+    public void onRadioBtnClik(View view){
+          // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+    Toast toast = null;
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rbResp1:
+                if (checked)
+                    // Pirates are the best
+                    strResp= (String)rbResp1.getText();
+                    //txtPrueba.setText(aciertos);
+                    break;
+            case R.id.rbResp2:
+                if (checked)
+                    strResp= (String)rbResp2.getText();
+               // txtPrueba.setText(aciertos);
+                    break;
+            case R.id.rbResp3:
+                if (checked)
+                    strResp= (String)rbResp3.getText();
+                //    txtPrueba.setText(aciertos);
+                    break;
+        }
     }
 }
