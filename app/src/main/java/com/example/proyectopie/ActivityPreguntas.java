91b4140d072c65801c66d4bcbde50e2f776c75c6
Ruben <ruben.menap@educa.jcyl.es>
2022-05-10 14:03:37 +0200

    BORRADO INNECESARIO

--- app/src/main/java/com/example/proyectopie/ActivityPreguntas.java
+++ app/src/main/java/com/example/proyectopie/ActivityPreguntas.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
 import java.util.ArrayList;
 
 public class ActivityPreguntas extends AppCompatActivity {
